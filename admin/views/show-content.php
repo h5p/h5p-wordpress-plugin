d17281c8d8bcbfec0b1a8eb95dd74cf76b860f5f
Frode Petterson <frode.andre.petterson@gmail.com>
2014-05-09 15:20:22 +0200

    Fixed h5p fullscreen in admin preview.

--- admin/views/show-content.php
+++ admin/views/show-content.php
@@ -13,5 +13,7 @@
 <div class="wrap">
   <h2><?php print esc_html($this->content['title']); ?><a href="<?php print admin_url('admin.php?page=h5p_new&id=' . $this->content['id']); ?>" class="add-new-h2"><?php esc_html_e('Edit', $this->plugin_slug); ?></a></h2>
   <?php print $this->print_messages(); ?>
-  <?php print $embed_code; ?>
+  <div class="h5p-wp-admin-wrapper">
+    <?php print $embed_code; ?>
+  </div>
 </div>
