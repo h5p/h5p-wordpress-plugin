a4eaf3d77b13034c8df9336c7788b9842a091c8a
Frode Petterson <frode.andre.petterson@gmail.com>
2016-03-18 14:13:12 +0100

    Include join and groupby in count query
    HFJ-1620

--- admin/class-h5p-content-query.php
+++ admin/class-h5p-content-query.php
@@ -230,7 +230,9 @@ class H5PContentQuery {
 
     $query = "SELECT COUNT(hc.id)
       FROM {$this->base_table}
-      {$this->where}";
+      {$this->join}
+      {$this->where}
+      GROUP BY hc.id";
 
     if (!empty($this->where_args)) {
       // We need to prep if we have args
