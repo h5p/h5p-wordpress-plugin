9b01cbb4b897842f7a3877c9ce843f2cc8853126
Frode Petterson <frode.andre.petterson@gmail.com>
2017-11-06 10:02:41 +0100

    Add doc for new action and update core

--- admin/class-h5p-plugin-admin.php
+++ admin/class-h5p-plugin-admin.php
@@ -201,6 +201,15 @@ class H5P_Plugin_Admin {
           $styles = array_merge($styles, $core->getAssetsUrls($files['styles']));
 
           $additional_embed_head_tags = array();
+
+          /**
+           * Add support for additional head tags for embedded content.
+           * Very useful when adding xAPI events tracking code.
+           *
+           * @since 1.9.5
+           *
+           * @param array &$additional_embed_head_tags
+           */
           do_action_ref_array('h5p_additional_embed_head_tags', array(&$additional_embed_head_tags));
 
           include_once(plugin_dir_path(__FILE__) . '../h5p-php-library/embed.php');
