9b36e7d729367b749814a97093063b152325fd41
Thomas Marstrander <marstranderthomas@gmail.com>
2020-08-26 16:28:48 +0200

    HFP-3008 Add content language to iframe

--- public/class-h5p-plugin.php
+++ public/class-h5p-plugin.php
@@ -960,18 +960,21 @@ class H5P_Plugin {
     // Getting author's user id
     $author_id = (int)(is_array($content) ? $content['user_id'] : $content->user_id);
 
+	  $metadata = $content['metadata'];
+	  $language = isset($metadata['defaultLanguage']) ? $metadata['defaultLanguage'] : 'en';
+
     // Add JavaScript settings for this content
     $settings = array(
       'library' => H5PCore::libraryToString($content['library']),
       'jsonContent' => $safe_parameters,
       'fullScreen' => $content['library']['fullscreen'],
       'exportUrl' => get_option('h5p_export', TRUE) ? $this->get_h5p_url() . '/exports/' . ($content['slug'] ? $content['slug'] . '-' : '') . $content['id'] . '.h5p' : '',
-      'embedCode' => '<iframe src="' . admin_url('admin-ajax.php?action=h5p_embed&id=' . $content['id']) . '" width=":w" height=":h" frameborder="0" allowfullscreen="allowfullscreen"></iframe>',
+      'embedCode' => '<iframe src="' . admin_url('admin-ajax.php?action=h5p_embed&id=' . $content['id']) . '" width=":w" height=":h" frameborder="0" allowfullscreen="allowfullscreen" lang="' . $language . '"></iframe>',
       'resizeCode' => '<script src="' . plugins_url('h5p/h5p-php-library/js/h5p-resizer.js') . '" charset="UTF-8"></script>',
       'url' => admin_url('admin-ajax.php?action=h5p_embed&id=' . $content['id']),
       'title' => $content['title'],
       'displayOptions' => $core->getDisplayOptionsForView($content['disable'], $author_id),
-      'metadata' => $content['metadata'],
+      'metadata' => $metadata,
       'contentUserData' => array(
         0 => array(
           'state' => '{}'
@@ -1042,7 +1045,10 @@ class H5P_Plugin {
       return '<div class="h5p-content" data-content-id="' . $content['id'] . '"></div>';
     }
     else {
-      return '<div class="h5p-iframe-wrapper"><iframe id="h5p-iframe-' . $content['id'] . '" class="h5p-iframe" data-content-id="' . $content['id'] . '" style="height:1px" src="about:blank" frameBorder="0" scrolling="no"></iframe></div>';
+      $language = isset($content['metadata']['defaultLanguage'])
+        ? $content['metadata']['defaultLanguage']
+        : 'en';
+      return '<div class="h5p-iframe-wrapper"><iframe id="h5p-iframe-' . $content['id'] . '" class="h5p-iframe" data-content-id="' . $content['id'] . '" style="height:1px" src="about:blank" frameBorder="0" scrolling="no" lang="' . $language . '"></iframe></div>';
     }
   }
 
