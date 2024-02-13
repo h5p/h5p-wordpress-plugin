528e71da2de164f3dc3b789fb1a96268b906262d
Oscar Arzola <oscararzola@gmail.com>
2024-02-13 16:50:53 -0500

    chore: code style

--- admin/class-h5p-plugin-admin.php
+++ admin/class-h5p-plugin-admin.php
@@ -49,7 +49,6 @@ class H5P_Plugin_Admin {
 
     /**
      * Keep track of the current privacy policy.
-     *
      * @since 1.1.0
      */
   private $privacy = NULL;
@@ -66,7 +65,7 @@ class H5P_Plugin_Admin {
 
     // Prepare admin pages / sections
     $this->content = new H5PContentAdmin($this->plugin_slug);
-    $this->library = new H5PLibrar  yAdmin($this->plugin_slug);
+    $this->library = new H5PLibraryAdmin($this->plugin_slug);
     $this->privacy = new H5PPrivacyPolicy($this->plugin_slug);
 
     // Initialize admin area.
